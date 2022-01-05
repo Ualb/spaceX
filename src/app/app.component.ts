443f9568a1b03e46bd298d4473855c2e75f27347
Ualb <ulises050794@gmail.com>
2022-01-04 20:16:28 -0600

    api spaceX consume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'space';
+  data: any[] = [];
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit() {
+    this.http.get('https://api.spacexdata.com/v3/rockets')
+    .subscribe((data: any) => {
+      this.data = data;
+    })
+  }
+
 }
