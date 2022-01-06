90b0cf25cc7a78d6799ec8388cf9342272c4dfb0
Ualb <ulises050794@gmail.com>
2022-01-05 20:41:26 -0600

    schema for the rockets data added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 
+import {Rocket} from '../schemas/rockets'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,17 +10,34 @@ import {HttpClient} from '@angular/common/http'
 })
 export class AppComponent {
   title = 'space';
-  data: any[] = [];
+  result:Rocket[] = new Array();
 
   constructor(private http: HttpClient){
 
   }
 
   ngOnInit() {
-    this.http.get('https://api.spacexdata.com/v3/rockets')
-    .subscribe((data: any) => {
-      this.data = data;
+    this.http.get('https://api.spacexdata.com/v4/rockets')
+    .subscribe((rockets: any) => {
+
+      const data:any[] = rockets; 
+      
+      this.result = data.map(e => {
+        return {
+          name: e.name,
+          description: e.description,
+          flickr_images: e.flickr_images
+        }
+      })
+
+      this.result.forEach(e => {
+        e.flickr_images = e.flickr_images.slice(0, 1);
+      })
+
+      console.log(this.result)
     })
+
+
   }
 
 }
